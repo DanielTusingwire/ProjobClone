c86701db44f3f5c56e7018e681bb0246fef15533
DanielTusingwire <dtusingwire17@gmail.com>
2023-10-02 19:05:26 +0300

    Worked on the middle secton by adding more content

--- Projobapp/src/Components/Home.js
+++ Projobapp/src/Components/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import HeroArrow from "../Assets/Arrow.svg"
 import Elips from "../Assets/Ellipse.svg"
-import image1 from '../Assets/Component.png'
+import image1 from '../Assets/Component.svg'
+import arrow8 from '../Assets/Arrow8.svg'
+import btnarrow from '../Assets/buttonarrow.svg'
 import "./HomeStyle.css";
 
 export const Home = () => {
@@ -31,8 +33,9 @@ export const Home = () => {
                 </div>
 
                 <div className='middleSection'>
+                <div className='middleSectionChild1'>
                     <div className='textContainer'>
-                        <h1>Launch Your Career with Confidence and Get Hired Faster!</h1>
+                        <h1>Launch Your Career with <span className='greencolor'>Confidence </span> and Get Hired <span className='greencolor'>Faster! </span></h1>
                         <p>Get Personalized Coaching from Successful Professionals to Help You Acquire the Skills and Confidence You Need to Secure Your First Job</p>
 
                     </div>
@@ -40,6 +43,29 @@ export const Home = () => {
                         <img src={image1}></img>
                         
                     </div>
+                    </div>
+
+                    <div className='middleSectionChild2'>
+                        <div className='bigtext'>
+                        <h1>Get a Powerful 
+                         Resume Guide for yourself , it’s Free! </h1>
+                        </div>
+                       
+                        <div className='arrow'>
+                        <img src={arrow8}></img>
+                        </div>
+
+                        <div className='button'>
+                        <h6> Click here to Get</h6>
+                        <img src={btnarrow}></img>
+                        </div>
+
+                         {/* <button className='btnclick'>Click here to Get <img src={btnarrow}/></button> */}
+
+                        
+                    </div>
+
+                    
                     
 
                 </div>
